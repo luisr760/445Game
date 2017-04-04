e2dfd2003d65ea97efae18e946894777d398956a
DESKTOP-9M5FT0C\Luis <l.rodriguez2@yahoo.com>
2017-04-04 12:39:37 -0700

    Start Here Commit

--- 445A/445/Assets/Scripts/Accelerometer.cs
+++ 445A/445/Assets/Scripts/Accelerometer.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Accelerometer : MonoBehaviour {
-	private float speed = .5f;
-
 	
-	// Update is called once per frame
+	private float speed = .5f;
 	void Update () 
 	{
-		
 		float temp = Input.acceleration.x;
 		transform.Translate (-temp * speed, 0, 0);
 	}
